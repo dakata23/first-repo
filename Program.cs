e672fa1587f9d220fc175ed119120e622ca8d4f9
dakata23 <47471622+dakata23@users.noreply.github.com>
2019-02-17 00:34:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha5_file_08_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            short age = short.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+            long egn = long.Parse(Console.ReadLine());
+            int employeeNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"First name: {firstName} \r\nLast name: {lastName} \r\nAge: {age} " +
+                $"\r\nGender: {gender} '\r\nPersonal ID: {egn} \r\nUnique Employee number: {employeeNumber}");
+        }
+    }
+}
